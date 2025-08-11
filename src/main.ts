88ed02c813eb86cd85987720dd1e43492b928769
tawan <thiago_tawan@outlook.com>
2025-08-11 13:46:33 -0300

    feat: api documentation

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { ValidationPipe } from '@nestjs/common';
 import { createDefaultUser } from './database/seeders/CreateDefaultUser';
 import { createDefaultCategory } from './database/seeders/CreateDefaultCategory';
 import { PrismaService } from './database/prisma.service';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { apiReference } from '@scalar/nestjs-api-reference';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +13,21 @@ async function bootstrap() {
   const prisma = app.get(PrismaService);
   await createDefaultUser(prisma);
   await createDefaultCategory(prisma);
+  const config = new DocumentBuilder()
+    .setTitle('Todo Api')
+    .setDescription('API documentation with Scalar')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  app.use(
+    '/docs',
+    apiReference({
+      content: document,
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
